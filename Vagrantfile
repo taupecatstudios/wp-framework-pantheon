2c8eb39c77815255f2a3f520e0cf7a29e9c97894
Tracy Rotton <trotton@taupecatstudios.com>
2018-11-26 21:09:22 -0500

    updated the Vagrant box to Bionic Beaver (18.04)

--- Vagrantfile
+++ Vagrantfile
@@ -36,8 +36,33 @@ Vagrant.configure("2") do |config|
   # https://docs.vagrantup.com.
 
   # Every Vagrant development environment requires a box. You can search for
-  # boxes at https://atlas.hashicorp.com/search.
-  config.vm.box = "ubuntu/xenial64"
+  # boxes at https://vagrantcloud.com/search.
+  config.vm.box = "ubuntu/bionic64"
+
+  # Disable automatic box update checking. If you disable this, then
+  # boxes will only be checked for updates when the user runs
+  # `vagrant box outdated`. This is not recommended.
+  # config.vm.box_check_update = false
+
+  # Create a forwarded port mapping which allows access to a specific port
+  # within the machine from a port on the host machine. In the example below,
+  # accessing "localhost:8080" will access port 80 on the guest machine.
+  # NOTE: This will enable public access to the opened port
+  # config.vm.network "forwarded_port", guest: 80, host: 8080
+
+  # Create a forwarded port mapping which allows access to a specific port
+  # within the machine from a port on the host machine and only allow access
+  # via 127.0.0.1 to disable public access
+  # config.vm.network "forwarded_port", guest: 80, host: 8080, host_ip: "127.0.0.1"
+
+  # Create a private network, which allows host-only access to the machine
+  # using a specific IP.
+  # config.vm.network "private_network", ip: "192.168.33.10"
+
+  # Create a public network, which generally matched to bridged network.
+  # Bridged networks make the machine appear as another physical device on
+  # your network.
+  # config.vm.network "public_network"
 
   config.hostmanager.enabled = true
   config.hostmanager.manage_host = true
@@ -81,8 +106,8 @@ Vagrant.configure("2") do |config|
 
   Vagrant.require_version(">= 2.1.1")
 
-  # Restart PHP and nginx after up to make sure symlinked config files are read
-  config.trigger.after [:up, :reload] do |trigger|
+    # Restart PHP and nginx after up to make sure symlinked config files are read
+    config.trigger.after [:up, :reload] do |trigger|
     trigger.run_remote = {inline: "service php7.2-fpm restart && service nginx restart"}
   end
 
--- scripts/provision.sh
+++ scripts/provision.sh
@@ -1,63 +1,64 @@
 #!/usr/bin/env bash
 
+echo "Provisioning"
+
 DB_NAME="##DB_NAME##"
 
-apt-get update >/dev/null 2>&1
-apt-get dist-upgrade -y >/dev/null 2>&1
+echo "OS Updates"
+apt-get update
+apt-get dist-upgrade -y
 
 echo "Installing MariaDB"
-debconf-set-selections <<< 'mysql-server mysql-server/root_password password password' >/dev/null 2>&1
-debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password password' >/dev/null 2>&1
-apt-get -y install mariadb-server >/dev/null 2>&1
+debconf-set-selections <<< 'mysql-server mysql-server/root_password password password'
+debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password password'
+apt-get -y install mariadb-server
 mysql --user=root --password=password -e \
-    "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY 'password' WITH GRANT OPTION; FLUSH PRIVILEGES;" >/dev/null 2>&1
-mysqladmin --user=root --password=password create $DB_NAME >/dev/null 2>&1
+    "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY 'password' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+mysqladmin --user=root --password=password create $DB_NAME
 
 echo "Installing nginx"
-apt-get install -y nginx >/dev/null 2>&1
+apt-get install -y nginx
 
-rm /etc/nginx/sites-enabled/default >/dev/null 2>&1
-ln -s /vagrant/scripts/default.conf /etc/nginx/sites-enabled/default >/dev/null 2>&1
+rm /etc/nginx/sites-enabled/default
+ln -s /vagrant/scripts/default.conf /etc/nginx/sites-enabled/default
 
-rm /etc/nginx/nginx.conf >/dev/null 2>&1
-ln -s /vagrant/scripts/nginx.conf /etc/nginx/nginx.conf >/dev/null 2>&1
+rm /etc/nginx/nginx.conf
+ln -s /vagrant/scripts/nginx.conf /etc/nginx/nginx.conf
 
-echo "Installing PHP 7.2"
-apt-get install python-software-properties >/dev/null 2>&1
-add-apt-repository ppa:ondrej/php >/dev/null 2>&1
-apt-get update
+echo "Installing PHP"
 apt-get install -y \
-    php7.2 \
-    php7.2-cli \
-    php7.2-gd \
-    php7.2-curl \
-    php7.2-memcache \
-    php7.2-fpm \
-    php7.2-xml \
-    php7.2-imagick \
-    php7.2-mbstring \
-    php7.2-mysql \
-    php7.2-xdebug \
-    php7.2-json \
-    >/dev/null 2>&1
+    ghostscript \
+    imagemagick \
+    php-cli \
+    php-curl \
+    php-fpm \
+    php-gd \
+    php-imagick \
+    php-json \
+    php-mbstring \
+    php-memcache \
+    php-mysql \
+    php-xdebug \
+    php-xml
 
-rm /etc/php/7.2/fpm/php.ini >/dev/null 2>&1
-ln -s /vagrant/scripts/php.ini /etc/php/7.2/fpm/php.ini >/dev/null 2>&1
+rm /etc/php/7.2/fpm/php.ini
+ln -s /vagrant/scripts/php.ini /etc/php/7.2/fpm/php.ini
 
-rm /etc/php/7.2/fpm/pool.d/www.conf >/dev/null 2>&1
-ln -s /vagrant/scripts/www.conf /etc/php/7.2/fpm/pool.d/www.conf >/dev/null 2>&1
+# Update ImageMagick policies so WordPress can create PDF thumbnail images.
+sed -i 's/rights="none" pattern="PDF"/rights="read|write" pattern="PDF"/g' /etc/ImageMagick-6/policy.xml
+sed -i 's/<\/policymap>/  <policy domain="coder" rights="read|write" pattern="LABEL" \/>\n<\/policymap>/g' /etc/ImageMagick-6/policy.xml
 
-service php7.2-fpm restart >/dev/null 2>&1
+rm /etc/php/7.2/fpm/pool.d/www.conf
+ln -s /vagrant/scripts/www.conf /etc/php/7.2/fpm/pool.d/www.conf
 
 echo "Installing PHPUnit"
-wget https://phar.phpunit.de/phpunit.phar >/dev/null 2>&1
-chmod +x phpunit.phar >/dev/null 2>&1
-mv phpunit.phar /usr/local/bin/phpunit >/dev/null 2>&1
+wget -nv https://phar.phpunit.de/phpunit.phar
+chmod +x phpunit.phar
+mv phpunit.phar /usr/local/bin/phpunit
 
 echo "Installing WP-CLI"
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar >/dev/null 2>&1
-chmod +x wp-cli.phar >/dev/null 2>&1
-mv wp-cli.phar /usr/bin/wp >/dev/null 2>&1
+curl -O -s https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+mv wp-cli.phar /usr/bin/wp
 
-echo "Reloading nginx configuration"
-service nginx reload >/dev/null 2>&1
+echo "cd /vagrant/web" >> /home/vagrant/.bashrc
